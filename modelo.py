5f890d65bb13c93f3c293dbf41c76a99677ebe48
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-13 11:20:28 -0300

    Relembrando POO - Alura
    Nesta aula, aprendemos sobre a construção de objetos e classes, utilizando encapsulamento. Vimos:
    
    - Criação da classe
    - Definição de métodos assessores 
    - @property
    - name

--- modelo.py
+++ modelo.py
@@ -1,17 +1,59 @@
 class Filme:
     def __init__(self,nome,ano,duracao):
-        self.nome = nome
+        self.__nome = nome
         self.ano = ano
         self.duracao = duracao
+        self.__like = 0
+
+    @property
+    def nome(self):
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__like
+
+    def dar_like (self):
+        self.__like += 1
+
 
 class Serie:
-    def __init__(self,nome,ano,temporadas):
-        self.nome = nome
+    def __init__(self, nome, ano, temporada):
+        self.__nome = nome
         self.ano = ano
-        self.temporadas = temporadas
+        self.temporada = temporada
+        self.__like = 0
 
-vingadores = Filme('Vigadores' , 2018 , 160)
-print(f'Nome:{vingadores.nome} - Ano:{vingadores.ano} - Duração:{vingadores.duracao}')
+    @property
+    def nome(self):
+        return self.__nome.title()
 
-atlanta = Serie('Atlanta' , 2018 , 2)
-print(f'Nome:{atlanta.nome} - Ano:{atlanta.ano} - Temporada:{atlanta.temporadas}')
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__like
+
+    def dar_like(self):
+        self.__like += 1
+
+
+vingadores = Filme('vingadores',2018,160)
+vingadores.dar_like()
+vingadores.nome = 'Mercenarios'
+
+print(f'Nome do filme: {vingadores.nome} - Ano : {vingadores.ano} - Duração : {vingadores.duracao}')
+print(f'Quantidade de likes : {vingadores.likes}')
+
+
+rick = Serie('Rick and Morty',2019,6)
+rick.dar_like()
+
+print(f'Nome do filme: {rick.nome} - Ano : {rick.ano} - Temporada : {rick.temporada}')
+print(f'Quantidade de likes : {rick.likes}')
