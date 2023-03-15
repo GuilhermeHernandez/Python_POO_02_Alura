0fd233d6e00c3ba2653cffa9c3d94428e1253878
Guilherme Hernandez Batista <68234095+GuilhermeHernandez@users.noreply.github.com>
2023-03-15 11:24:33 -0300

    Aprofundando em POO
    Nesta aula, conseguimos fazer com que o código duplicado fosse eliminado. Aprendemos os seguintes conceitos:
    
    Herança
    Generalização/especialização
    Método super()
    Polimorfismo 
    Representar um objeto no formato String com o __str__

--- modelo.py
+++ modelo.py
@@ -19,15 +19,27 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def imprimir(self):
+        print(f'Nome: {self._nome} - {self.ano} Ano - Likes: {self._likes}')
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    #Função que retorna o objeto como string
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.duracao} Duração - {self.ano} Ano - Likes: {self._likes}'
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    # Polimorfismo da função
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.temporadas} Temporadas - {self.ano} Ano - Likes: {self._likes}'
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -39,5 +51,8 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]  #Lista recebe qualquer tipo de dados
+
+for programa in filmes_e_series:
+    print(programa)
